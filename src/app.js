c6d953464a7a0de2173fe31a3200e212d0b8e355
Eitan31 <145723798+Eitan31@users.noreply.github.com>
2023-10-04 01:43:07 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,27 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generate();
+  });
+  document.querySelector("#excuse").innerHTML = generate();
+};
+let generate = () => {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoIndex = who[Math.floor(Math.random() * who.length)];
+  let actionIndex = action[Math.floor(Math.random() * action.length)];
+  let whatIndex = what[Math.floor(Math.random() * what.length)];
+  let whenIndex = when[Math.floor(Math.random() * when.length)];
+  let total = whoIndex + " " + actionIndex + " " + whatIndex + " " + whenIndex;
+  return total;
 };
